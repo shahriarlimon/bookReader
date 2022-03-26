f7548521235bb150fbab10a847f78d4721727dbd
shahriarlimon <shahriarwebdev@gmail.com>
2022-03-26 10:07:18 +0600

    added and implemented rest functionality

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {AiOutlineDelete} from 'react-icons/ai';
 
-const Cart = ({cart}) => {
+const Cart = ({cart,handleResetCart,handleSelectOneCart}) => {
     return (
-        <div className='sticky top-0 p-5'>
-            <h1 className='md:text-2xl text-md font-bold'>Selected Book</h1>
+        <div className='sticky top-0 p-5 bg-gray-200 rounded shadow-lg'>
+            <h1 className='md:text-2xl text-md font-bold'>Selected Books</h1>
            <div className='mt-5'>
            {
                 cart.map(book=><div key={book.id} className='flex items-center space-x-1 md:space-x-3'>
@@ -15,8 +15,9 @@ const Cart = ({cart}) => {
             }
            </div>
            <div className='mt-2 flex flex-col'>
-               <button className='w-full md:w-1/2 px-1 py-1 md:px-3 md:py-2 bg-green-500 text-white hover:bg-green-700 rounded text-sm'>Choose 1 for Me</button>
-               <button className='w-full md:w-1/2  px-1 py-1 md:px-3 md:py-2 bg-pink-500 text-white hover:bg-pink-700 rounded mt-3'>Choose Again</button>
+               <button onClick={()=>handleSelectOneCart(cart)} className='w-full md:w-1/2 px-1 py-1 md:px-3 md:py-2 bg-green-500 text-white hover:bg-green-700 rounded text-sm'>Select 1 for Me</button>
+               <button onClick={()=>handleResetCart(cart)} className='w-full md:w-1/2  px-1 py-1 md:px-3 md:py-2 bg-pink-500 text-white hover:bg-pink-700 rounded mt-3'> Reset 
+               </button>
            </div>
             
         </div>
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -14,6 +14,15 @@ const Shop = () => {
         const newCart = [...cart, selectedBook];
         setCart(newCart);
     }
+    const handleSelectOneCart = (cart)=>{
+        const indexCart = Math.floor((Math.random() * cart.length) + 1);
+        const newIndexCart = cart[indexCart-1];
+        const newCart = [newIndexCart];
+        setCart(newCart);
+    }
+    const handleResetCart= (cart)=>{
+        setCart([])
+    }
     return (
         <div className='grid grid-cols-2 md:grid-cols-5 md:gap-10 w-full min-h-screen'>
             <div className=' col-span-1 md:col-span-3 grid md:grid-cols-2 grid-cols-1 gap-5 md:gap-8 '>
@@ -21,8 +30,8 @@ const Shop = () => {
                     books.map(book=><Book handleAddToCart={handleAddToCart} key={book.id} book={book}/>)
                 }
             </div>
-            <div className=' ml-3 col-span-1 md:col-span-2 bg-blue-100 relative md:px-8 md:py-5 '>
-                <Cart cart={cart}/>
+            <div className=' ml-3 col-span-1 md:col-span-2 relative md:px-8 md:py-5 '>
+                <Cart handleSelectOneCart={handleSelectOneCart} handleResetCart={handleResetCart} cart={cart}/>
             </div>
         </div>
     );
